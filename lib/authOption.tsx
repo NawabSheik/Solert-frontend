e1f8dfcf2ed7961e69954b2b279a3c63aba1c493
NawabSheik <nawabsheikh7105@gmail.com>
2024-08-29 13:06:54 +0530

    Auth Options Production Fixed

--- lib/authOption.tsx
+++ lib/authOption.tsx
@@ -1,15 +1,14 @@
-import axios from "axios";
+import nextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { signIn, signOut } from "next-auth/react";
-
+import axios from "axios";
 
 const authOptions = {
     providers: [
         CredentialsProvider({
             name: "credentials",
             credentials: {
-                username: { label: "Email", type: "text"},
-                password: { label: "Password", type: "password"}
+                username: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
             },
             async authorize(credentials: any, req: any): Promise<any> {
                 const { email, password } = credentials;
@@ -19,7 +18,10 @@ const authOptions = {
                 }
 
                 try {
-                    const response = await axios.post('http://ec2-43-205-127-140.ap-south-1.compute.amazonaws.com/api/v1/users/login', { email, password })
+                    const response = await axios.post(
+                        'http://ec2-43-205-127-140.ap-south-1.compute.amazonaws.com/api/v1/users/login',
+                        { email, password }
+                    );
                     const user = response.data;
 
                     if (!user) {
@@ -55,11 +57,16 @@ const authOptions = {
     pages: {
         signIn: '/login',
         signOut: '/',
-        Error: '/'
+        error: '/' // lowercase 'e' here; 'Error' should be 'error'
     },
+    
+    // This is the correct type for the session option
     session: {
-        strategy:"jwt"
+        strategy: "jwt"
     }
+};
 
-}
+const handler = nextAuth(authOptions);
+
+export { handler as GET, handler as POST };
 export default authOptions;
